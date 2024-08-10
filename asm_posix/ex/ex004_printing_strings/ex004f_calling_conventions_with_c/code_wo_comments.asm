3cdd56ff2881d67d215036721a6789dc98837114
Lloyd Lobo <lloydlobo4@gmail.com>
2024-08-10 07:14:42 +0530

    Redo print label again without help

--- asm_posix/ex/ex004_printing_strings/ex004f_calling_conventions_with_c/code_wo_comments.asm
+++ asm_posix/ex/ex004_printing_strings/ex004f_calling_conventions_with_c/code_wo_comments.asm
@@ -1,11 +1,11 @@
 	; *---------------------------------------------------------------------------------------
-	; * file: code.asm
 	; *
 	; * A 64-bit Linux application that writes the first 90 Fibonacci numbers.
 	; *
 	; *     nasm -f elf64 code.asm && zig cc code.o && ./a.out
 	; *
 	; * Ported from "[Understanding Calling Conventions](https://cs.lmu.edu/~ray/notes/nasmtutorial/)"
+	; *
 	; *----------------------------------------------------------------------------------------
 
 	global main
@@ -28,40 +28,37 @@ format:
 main:
 	push rbx
 	mov  rdi, progn
-	call puts; write program name to stdout (standard output)
+	call puts
 
-	mov ecx, 90; counter
-	xor rax, rax; current_number
-	xor rbx, rbx; next_number
-	inc rbx; {rbx} is originally 1
+	mov ecx, 90
+	xor rax, rax
+	xor rbx, rbx
+	inc rbx
 
 print:
-._save:
+.save:
 	push rax
 	push rcx
 
-._call:
+.load:
 	mov  rdi, format
 	mov  rsi, rax
 	xor  rax, rax
 	call printf
 
-._restore:
+.restore:
 	pop rcx
 	pop rax
 
-._next:
-	;   algorithm: next = curr + prev = n-1 + n-2
-	mov rdx, rax; temp = curr
-	mov rax, rbx; curr = next
-	add rbx, rdx; next += temp
-	dec ecx; counter--
-	jnz print; goto `print:` if {ecx} is not zero
-	;^  if not done counting, do some more (loop again)
-	;|  `jnz` - if the processor's `Z` (zero) flag is set to 0, jump to the given label.
-	;|  `jz`or`je` - if the processor's `ZF` (zero) flag is set to 1, jump to the given label.
+.next:
+	mov rdx, rax
+	mov rax, rbx
+	add rbx, rdx
+	dec ecx
+	jnz print
 
 leave:
 	pop rbx
 	mov rax, 0
 	ret
+
