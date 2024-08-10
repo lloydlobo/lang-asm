cbcb32216ca710ba4ae05386f9d2c5bb12d05120
Lloyd Lobo <lloydlobo4@gmail.com>
2024-08-10 11:45:16 +0530

    Tidy up

--- asm_posix/ex/ex005_c_and_assembly/ex005e_command_line_arguments/code.asm
+++ asm_posix/ex/ex005_c_and_assembly/ex005e_command_line_arguments/code.asm
@@ -12,59 +12,28 @@
 	; *----------------------------------------------------------------------------------------
 
 	;----------------------------------------------------------------------
-	; DEFINITIONS
+	;        DEFINITIONS
 	;----------------------------------------------------------------------
-
-	%define EXIT_SUCCESS 0
-	%define EXIT_FAILURE 1
-
+	%define  EXIT_SUCCESS 0
+	%define  EXIT_FAILURE 1
 	;----------------------------------------------------------------------
-	; INCLUDES
+	;        INCLUDES
 	;----------------------------------------------------------------------
-
 	%include "syscalls.asm"; Requires syscall listing for your OS in lib/sys/
-
 	%include "lib/sys/exit.asm"; void exit(byte {dil})
-
 	;----------------------------------------------------------------------
-	; INSTRUCTIONS
+	;        INSTRUCTIONS
 	;----------------------------------------------------------------------
-
-	; By the x86 64-bit calling convention, the first six arguments are
-	; passed in the registers {rdi} {rsi} {rdx} {rcx} {r8} {r9} in that
-	; order. {rax} contains the system call ordinal.
-
-	; In x86: EBX(fd) ECX(buffer) EDX(length) EAX(system call ordinal)
-	; In x64: RDI RSI RDX RCX RAX(system call ordinal)
-
-	global main
-	extern puts
-
-	; * NOTE:
-	; * before adding section .bss, output of `print_progn:`:
-	; *     program: ex005e_command_line_arguments
-	; *     Linker: LLD 18.1.6
-	; * and after adding section .bss, output of `print_progn:`:
-	; *     program: ex005e_command_line_arguments
-
-	;       SECTION `.data`
-	section .bss
-	;       #--
-
-	;       SECTION `.data`
-	section .data
-
-progn:
-	db "program: ex005e_command_line_arguments"; note: omit line-feed as `puts` does it for us
-	;  `db "...", 0x0a`; 10 is line-feed `\n`; (10 or 0x0a or 0XA -> `\n`)
-
-	;       SECTION `.text`
-	section .text
-
-print_progn:
-	mov  rdi, progn
-	call puts
-	ret
+	;        * By the x86 64-bit calling convention, the first six arguments are
+	;        * passed in the registers {rdi} {rsi} {rdx} {rcx} {r8} {r9} in that
+	;        * order. {rax} contains the system call ordinal.
+	;        *
+	;        * In x86: EBX(fd) ECX(buffer) EDX(length) EAX(system call ordinal)
+	;        * In x64: RDI RSI RDX RCX RAX(system call ordinal)
+	global   main
+	extern   puts
+	;        SECTION `.text`
+	section  .text
 
 	; * You know that in C, main is just a plain old function, and it has a
 	; * couple parameters of its own:
@@ -76,24 +45,43 @@ print_progn:
 	; * echo the command line arguments to a program, one per line:
 
 echo_argv:
-	; * Function: `void {rax} echo_argv(long {rdi}, long *{rsi})`
-	; * Arguments:
-	; * - {rdi}: argument count passed to command line (argc)
-	; * - {rsi}: address of string to output, i.e arguments passed to command
-	; * line (argv)
-
+	;    * Function: `void {rax} echo_argv(long {rdi}, long *{rsi})`
+	;    * Arguments:
+	;    * - {rdi}: argument count passed to command line (argc)
+	;    * - {rsi}: address of string to output, i.e arguments passed to
+	;    command
+	;    * line (argv)
 	push rdi; save registers that `puts` uses
 	push rsi
 	sub  rsp, 8; must align stack before call
-
-	mov  rdi, [rsi]
-	call puts
-
-	add rsp, 8; restore {rsp} to pre-aligned value
-	pop rsi; restore registers that `puts` used
-	pop rdi
-	jmp .next_argv_elem_inc_rsi
+	mov  rdi, [rsi]; <<< actual work
+	call puts; <<< actual work
+	add  rsp, 8; restore {rsp} to pre-aligned value
+	pop  rsi; restore registers that `puts` used
+	pop  rdi
+	jmp  .next_argv_elem_inc_rsi
 	ret ; else return to label `main`
+
+.next_argv_elem_inc_rsi:
+	add rsi, 8; point to the next argument in {rsi}
+	;   i.e. long *{rsi} or `char *argv[]`
+	dec rdi; count down {rdi} i.e. `int argc`
+	jnz echo_argv; if not done counting, keep going
+	ret ; return
+
+print_progn:
+	mov  rdi, progn
+	call puts
+	ret
+
+main:
+	push rdi; save registers that `puts` uses
+	push rsi
+	call print_progn
+	pop  rsi; restore registers that `puts` used
+	pop  rdi
+	jmp  echo_argv
+	ret ; return to `main` call site in C
 
 	; * Output:
 	; * ./a.out
@@ -102,20 +90,23 @@ echo_argv:
 	; * -zzz
 	; * Hellope
 
-.next_argv_elem_inc_rsi:
-	add rsi, 8; point to the next argument in {rsi} i.e. long *{rsi} or `char *argv[]`
-	dec rdi; count down {rdi} i.e. `int argc`
-	jnz echo_argv; if not done counting, keep going
-	ret ; return
+	;       SECTION `.bss`
+	section .bss
+	;       * ---
 
-main:
-	push rdi; save registers that `puts` uses
-	push rsi
-	call print_progn
-	pop  rsi; restore registers that `puts` used
-	pop  rdi
+	; ^
+	; * NOTE:
+	; * before adding section .bss, output of `print_progn:`:
+	; *     program: ex005e_command_line_arguments
+	; *     Linker: LLD 18.1.6
+	; * and after adding section .bss, output of `print_progn:`:
+	; *     program: ex005e_command_line_arguments
 
-	jmp echo_argv
+	;       SECTION `.data`
+	section .data
 
-.leave:
-	ret; return
+progn:
+	db "program: ex005e_command_line_arguments"; note: omit line-feed as `puts`
+	;  does it for us
+	;  `db "...", 0x0a`; 10 is line-feed `\n`; (10 or 0x0a or 0XA -> `\n`)
+
