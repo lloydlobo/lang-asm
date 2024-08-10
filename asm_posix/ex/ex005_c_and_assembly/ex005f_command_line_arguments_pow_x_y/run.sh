0c0c0e36c239ccbaee4107ced98df766eba972f1
Lloyd Lobo <lloydlobo4@gmail.com>
2024-08-10 16:16:33 +0530

    Parse command line integer args and calculate pow

---
+++ asm_posix/ex/ex005_c_and_assembly/ex005f_command_line_arguments_pow_x_y/run.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# file: run.sh
+#
+# watch:
+# : find . -name '*.asm' | entr -rs "date;nasm -f elf64 code.asm && zig cc code.o && ./a.out && echo;echo exit status $?;"
+# : find -name '*.asm' | entr -crs 'date;./run.sh'
+
+set -xe
+
+nasm -f elf64 code.asm \
+	-I ../../../ -I "../../../lib/sys/$(uname)" \
+	-Wlabel-orphan -Wno-orphan-labels \
+	-O0 -g -F dwarf
+#   -Ox
+
+: exit status $?
+
+# when C interop:
+# : ld -o binary code.o -lc -s
+# else:
+zig cc code.o
+: exit status $?
+
+hexdump -C a.out >code.asm.hex.dump &
+: exit status $? &
+
+objdump -M intel -d a.out >code.asm.a.out.obj.dump &
+: exit status $? &
+
+strace -c ./a.out 2 19 &
+: exit status $? &
+
+./a.out 2 19 #> 524288
+: exit status $?
+
+# FIN
